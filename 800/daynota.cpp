31e67a56504bc9eeae9191bdf1b34be522137aba
Shimanto9sqr <119039604+Shimanto9sqr@users.noreply.github.com>
2024-03-29 19:06:44 +0600

    seems to be canny

---
+++ 800/daynota.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+using ll = long long;
+ 
+ 
+ 
+void solve(){
+    int n,k; bool isExist= false;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(k==arr[i]){
+            
+            isExist=true;
+            break;
+            
+        }
+    }
+    if(isExist){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+    return;
+}
+int main()
+{
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        solve();
+    }
+    return 0;
+}
